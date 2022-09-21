546b5f36812b04117f4df7a178be425a85ad1c08
tch <amazingtch@foxmail.com>
2022-09-21 20:03:21 +0800

    折叠表达式应用。

--- 04VariadicTemplates/P058.FoldExpression.cpp
+++ 04VariadicTemplates/P058.FoldExpression.cpp
@@ -33,9 +33,21 @@ void print2(Args... args)
     std::cout << std::endl;
 }
 
+// comma operator, no overhead comapred to &&
+template<typename... Args>
+void print3(Args... args)
+{
+    auto printSpace = [](std::ostream& os, const auto& val) -> void {
+        os << val << " ";
+    };
+    (... , printSpace(std::cout, args));
+    std::cout << std::endl;
+}
+
 int main(int argc, char const *argv[])
 {
     print1(1, 2, "hello", 3.01);
     print2(1, 2, "hello", 3.01);
+    print3(1, 2, "hello", 3.01);
     return 0;
 }
